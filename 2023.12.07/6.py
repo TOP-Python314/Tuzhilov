ff72a5f9c9cf0c8e6d437852fe1b506da5df483c
urObOrOs <andrygolova4@gmail.com>
2024-04-23 00:15:51 +0500

    HoWork 7.12.23 update

---
+++ 2023.12.07/6.py
@@ -0,0 +1,13 @@
+bam_bam = {'0', '1', '0b', 'b'}
+
+a = input( )
+
+for number in bam_bam: 
+    if set(bam_bam) and set(a):
+        print('да')
+        break
+    else:
+        print('нет')
+        break
+    
+    
